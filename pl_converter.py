1eaa361686318e3c06359d65e6efcf93543d5c2a
Tahir Jalilov <tahir.jalilov@gmail.com>
2020-04-26 23:09:06 +0400

    Done

--- pl_converter.py
+++ pl_converter.py
@@ -19,25 +19,28 @@ def get_play_list(url):
 
 
 def converter(pl_lines):
-    play_list = f'#EXTM3U x-tvg-url="{config.EPG}"\n\n'
-    prefix = ''
-    chanel_name = ''
-    chanel_group = ''
+    play_list = f'#EXTM3U x-tvg-url="{config.EPG_URL}"\n\n'
+    prefix = ""
+    channel_name = ""
+    channel_group = ""
     if next(pl_lines) != '#EXTM3U':  # checking first line for existing of #EXTM3U
         return False
 
     for line in pl_lines:
-        if line[:7] == '#EXTINF':
+        if line[:7] == "#EXTINF":
             parts = line.split(',')
             prefix = parts[0].strip(' \t\n')
-            chanel_name = parts[1].strip(' \t\n')
-        elif line[:7] == '#EXTGRP':
+            channel_name = parts[1].strip(' \t\n')
+        elif line[:7] == "#EXTGRP":
             parts = line.split(':')
-            chanel_group = parts[1].strip(' \t\n')
-            # play_list += f'{prefix} tvg-name="{chanel_name}", group-title="{chanel_group}", {chanel_name}\n'
+            channel_group = parts[1].strip(' \t\n')
         elif line[:4] == "http":
             link = line.strip(' \t\n')
-            play_list += f'{prefix} tvg-name="{chanel_name}", group-title="{chanel_group}", {chanel_name}\n{link}\n\n'
+            # channel_id = link.split("/")[5]
+            play_list += f'{prefix} group-title="{channel_group}", tvg-name="{channel_name}", ' \
+                         f'{channel_name}\n{link}\n\n'
+            # play_list += f'{prefix} group-title="{channel_group}", tvg-id="{channel_id}", ' \
+            #              f'tvg-logo="{config.LOGO_URL}{channel_id}.png",{channel_name}\n{link}\n\n'
         else:
             pass
     return play_list
@@ -49,7 +52,7 @@ def gen_file(play_list):
 
 
 if __name__ == '__main__':
-    old_play_list = get_play_list(config.URL)
+    old_play_list = get_play_list(config.PL_URL)
     new_play_list = converter(old_play_list)
     if new_play_list:
         gen_file(new_play_list)
