95b7d240ab20c6074a7335457508efccedf314cb
Tahir Jalilov <tahir.jalilov@gmail.com>
2020-12-28 21:01:07 +0400

    code refactored

--- pl_converter.py
+++ pl_converter.py
@@ -1,54 +1,74 @@
 # -*- coding: utf-8 -*-
+"""Main module."""
 
 import requests
-import config
+import toml
+
+config = toml.load('config.toml')
 
 
 def get_play_list(url):
-    result = requests.get(url, stream=True)
-    result.encoding = 'utf-8'
-    lines = result.iter_lines(decode_unicode=True)
-    return lines
+    """Get playlist from URL.
+
+    Args:
+        url: playlist url
+
+    Returns:
+        lines: generator object of a playlist lines
+    """
+    playlist = requests.get(url, stream=True)
+    playlist.encoding = 'utf-8'
+    return playlist.iter_lines(decode_unicode=True)
 
 
-def converter(pl_lines):
-    play_list = f'#EXTM3U x-tvg-url="{config.EPG_URL}"\n\n'
-    prefix = ""
-    channel_name = ""
-    channel_group = ""
-    if next(pl_lines) != '#EXTM3U':  # checking first line for existing of #EXTM3U
+def playlist_converter(playlist_lines):
+    """Convert playlist to the format that Kodi supports.
+
+    Args:
+        playlist_lines: generator object of a playlist lines
+
+    Returns:
+        playlist: Converted playlist
+    """
+    playlist = '#EXTM3U x-tvg-url="{0}"\n'.format(config['epg']['url'])
+    prefix = ''
+    channel_name = ''
+    channel_group = ''
+    if next(playlist_lines) != '#EXTM3U':
         return False
 
-    for line in pl_lines:
-        if line[:7] == "#EXTINF":
+    for line in playlist_lines:
+        if line[:7] == '#EXTINF':
             parts = line.split(',')
-            prefix = parts[0].strip(' \t\n')
-            channel_name = parts[1].strip(' \t\n')
-        elif line[:7] == "#EXTGRP":
+            prefix = parts[0].strip()
+            channel_name = parts[1].strip()
+        elif line[:7] == '#EXTGRP':
             parts = line.split(':')
-            channel_group = parts[1].strip(' \t\n')
-        elif line[:4] == "http":
-            link = line.strip(' \t\n')
-            # channel_id = link.split("/")[5]
-            play_list += f'{prefix} group-title="{channel_group}", tvg-name="{channel_name}", ' \
-                         f'{channel_name}\n{link}\n\n'
-            # play_list += f'{prefix} group-title="{channel_group}", tvg-id="{channel_id}", ' \
-            #              f'tvg-logo="{config.LOGO_URL}{channel_id}.png",{channel_name}\n{link}\n\n'
-        else:
-            pass
-    return play_list
+            channel_group = parts[1].strip()
+        elif line[:4] == 'http':
+            url = line.strip()
+            playlist += '{0} group-title="{1}", tvg-name="{2}" {3}\n{4}\n'.format(
+                prefix,
+                channel_group,
+                channel_name,
+                channel_name,
+                url,
+                )
+    return playlist
 
 
-def gen_file(play_list):
+def generate_file(playlist: str):
+    """Generate new playlist file.
+
+    Args:
+        playlist: Playlist
+    """
     with open('new_playlist.m3u8', 'w') as output_file:
-        output_file.write(play_list)
+        output_file.write(playlist)
 
 
 if __name__ == '__main__':
-    old_play_list = get_play_list(config.PL_URL)
-    new_play_list = converter(old_play_list)
+    old_play_list = get_play_list(config['playlist']['url'])
+    new_play_list = playlist_converter(old_play_list)
     if new_play_list:
-        gen_file(new_play_list)
-        print("new playlist is ready")
-    else:
-        print("Incorrect playlist format")
+        generate_file(new_play_list)
